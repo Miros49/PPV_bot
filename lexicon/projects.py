d627247bca1235bb7ea00c5bd33d9c868e28d8a0
Lebowski66 <118187778+Lebowski66@users.noreply.github.com>
2024-07-26 22:10:49 +0300

    окончательные проекты и сервера

--- lexicon/projects.py
+++ lexicon/projects.py
@@ -19,28 +19,23 @@ PROJECTS = {
 
 
 
-# GTA5_PROJECTS = [
-#     'GTA5RP', 'Majestic', 'Grand RP',
-#     'Radmir RP', 'ARIZONA V', 'RMRP', 'QUANT RP', 'SMOTRArage', 'GALAXY DM',
-# ]
+GTA5
 
-# OTHER_PROJECTS = [
-#     'BLACK RUSSIA', 'Arizona RP', 'МАТРЁШКА RP', 'GRAND MOBILE', 'HASSLE ONLINE (Radmir RP)', 'Родина РП','Next RP',
-#     'Amazing Online','MTA Province','Absolute Play',
-#     'Малиновка RP'
-# ]
+GTA5RP (сервера есть)
+Majestic RP (сервера есть)
+Radmir RP (сервера есть + МОЙ ДОМ)
+RMRP (есть сервера)
+Arizona V (есть сервера) 
+Grand RP (Оставить только русские (2 сервера))
+SMOTRArage (Один сервер у проекта) 
+Quant RP (Один сервер у проекта) 
 
-
-# PROJECTS = {
-#     'gta5': GTA5_PROJECTS,
-#     'other': OTHER_PROJECTS
-# }
-
-# # QUANT RP, SMOTRArage, GALAXY DM,  - у проектов 1 сервер (можешь сделать, что просто выбрали проект и не выводится выбор сервера (т.к он один))
-
-# # Малиновка - Server 1,2,3,4
-# # Absolute Play - Титаниум , Литиум , Аурум , Хромиум , Платинум , DM Play
-# # MTA Province -  Server 1,2,3,4,5,6,7
-# # Amazing Online - RED, YELLOW, GREEN, AZURE, SILVER, ROSE, BLACK
-# # Родина РП - Центральный округ, Южный округ, Северный округ, Восточный округ, Западный округ, Приморский округ, Федеральный округ
-# # МАТРЕШКА RP - Server 1,2...30
+SAMP/MOBILE/CRMP
+BLACK RUSSIA (есть сервера + 76. Chita 77. Kostroma)
+ARIZONA (+ 28. Christmas, 29. Mirage, 30. Love)
+GRAND MOBILE (1-33)
+МАТРЕШКА (1-30)
+HASSLE ONLINE (RADMIR) (1-21)
+RODINA RP - Центральный округ, Южный округ, Северный округ, Восточный округ, Западный округ, Приморский округ, Федеральный округ
+Amazing RP (Red, Yellow, Green, Azure, Silver, Rose, Black)
+NEXTRP (есть сервера)
