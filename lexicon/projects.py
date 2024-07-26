7eff8a45fd2b4293ffc483437cd964c1f59d65d3
Lebowski66 <118187778+Lebowski66@users.noreply.github.com>
2024-07-26 22:12:27 +0300

    окончательные проекты и сервера

--- lexicon/projects.py
+++ lexicon/projects.py
@@ -32,9 +32,9 @@ Quant RP (Один сервер у проекта)
 
 SAMP/MOBILE/CRMP
 BLACK RUSSIA (есть сервера + 76. Chita 77. Kostroma)
-ARIZONA (+ 28. Christmas, 29. Mirage, 30. Love)
+ARIZONA RP (+ 28. Christmas, 29. Mirage, 30. Love)
 GRAND MOBILE (1-33)
-МАТРЕШКА (1-30)
+МАТРЕШКА RP (1-30)
 HASSLE ONLINE (RADMIR) (1-21)
 RODINA RP - Центральный округ, Южный округ, Северный округ, Восточный округ, Западный округ, Приморский округ, Федеральный округ
 Amazing RP (Red, Yellow, Green, Azure, Silver, Rose, Black)
